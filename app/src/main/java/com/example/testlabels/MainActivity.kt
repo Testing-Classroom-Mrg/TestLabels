a21a0fad59ca00aa8eb2b770cf0a4be812ddaa55
giannispapageorgiou <g.papageorgiou@thebeat.co>
2020-06-04 13:26:58 +0300

    test commit

--- app/src/main/java/com/example/testlabels/MainActivity.kt
+++ app/src/main/java/com/example/testlabels/MainActivity.kt
@@ -2,11 +2,16 @@ package com.example.testlabels
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ActionMode
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onActionModeFinished(mode: ActionMode?) {
+        super.onActionModeFinished(mode)
     }
 }
