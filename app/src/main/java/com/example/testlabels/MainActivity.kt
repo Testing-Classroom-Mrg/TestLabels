a630816e2cdc6ea2401c4f9af9f0b70aca565d8e
giannispapageorgiou <g.papageorgiou@thebeat.co>
2020-06-04 13:28:31 +0300

    actor var

--- app/src/main/java/com/example/testlabels/MainActivity.kt
+++ app/src/main/java/com/example/testlabels/MainActivity.kt
@@ -11,7 +11,4 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
-    override fun onActionModeFinished(mode: ActionMode?) {
-        super.onActionModeFinished(mode)
-    }
 }
