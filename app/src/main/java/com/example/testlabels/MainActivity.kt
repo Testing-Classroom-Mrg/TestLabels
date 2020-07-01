dc320e41cf7f45309d47c1f231c90aa4409d93f3
giannispapageorgiou <g.papageorgiou@thebeat.co>
2020-07-01 16:42:16 +0300

    test

--- app/src/main/java/com/example/testlabels/MainActivity.kt
+++ app/src/main/java/com/example/testlabels/MainActivity.kt
@@ -13,8 +13,4 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
-    override fun onApplyThemeResource(theme: Resources.Theme?, resid: Int, first: Boolean) {
-        super.onApplyThemeResource(theme, resid, first)
-    }
-
 }
