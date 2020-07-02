ac9a97d045bbe96b8a00140165dd7f4b9dc5afc6
giannispapageorgiou <g.papageorgiou@thebeat.co>
2020-07-02 11:49:16 +0300

    add lines

--- app/src/main/java/com/example/testlabels/MainActivity.kt
+++ app/src/main/java/com/example/testlabels/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.testlabels
 
 import android.os.Bundle
+import android.view.ActionMode
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -8,5 +9,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    //lines
+
+    override fun onActionModeStarted(mode: ActionMode?) {
+        super.onActionModeStarted(mode)
     }
 }
