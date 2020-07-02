bb4df73ff0ee925f982fa69581ce26cf34b72a96
giannispapageorgiou <g.papageorgiou@thebeat.co>
2020-07-02 11:33:30 +0300

    add lines

--- app/src/main/java/com/example/testlabels/MainActivity.kt
+++ app/src/main/java/com/example/testlabels/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.testlabels
 
+import android.app.Activity
+import android.content.Intent
+import android.content.res.Resources
 import android.os.Bundle
+import android.view.ActionMode
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -8,5 +12,29 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onActionModeFinished(mode: ActionMode?) {
+        super.onActionModeFinished(mode)
+    }
+
+    override fun onActivityReenter(resultCode: Int, data: Intent?) {
+        super.onActivityReenter(resultCode, data)
+    }
+
+    override fun onActionModeStarted(mode: ActionMode?) {
+        super.onActionModeStarted(mode)
+    }
+
+    override fun onApplyThemeResource(theme: Resources.Theme?, resid: Int, first: Boolean) {
+        super.onApplyThemeResource(theme, resid, first)
+    }
+
+    override fun onAttachedToWindow() {
+        super.onAttachedToWindow()
+    }
+
+    override fun onChildTitleChanged(childActivity: Activity?, title: CharSequence?) {
+        super.onChildTitleChanged(childActivity, title)
     }
 }
