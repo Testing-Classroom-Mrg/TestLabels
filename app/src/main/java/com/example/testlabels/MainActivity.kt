fc77a13fed6991ebf110779bc685c5080fa7ffa5
giannispapageorgiou <g.papageorgiou@thebeat.co>
2020-07-02 11:05:17 +0300

    some line additions

--- app/src/main/java/com/example/testlabels/MainActivity.kt
+++ app/src/main/java/com/example/testlabels/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.example.testlabels
 
+import android.content.Intent
+import android.content.ServiceConnection
+import android.content.res.Configuration
 import android.content.res.Resources
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.view.ActionMode
+import java.util.concurrent.Executor
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,6 +19,24 @@ class MainActivity : AppCompatActivity() {
 
     override fun onApplyThemeResource(theme: Resources.Theme?, resid: Int, first: Boolean) {
         super.onApplyThemeResource(theme, resid, first)
+    }
+
+    override fun applyOverrideConfiguration(overrideConfiguration: Configuration?) {
+        super.applyOverrideConfiguration(overrideConfiguration)
+    }
+
+    override fun onActionModeFinished(mode: ActionMode?) {
+        super.onActionModeFinished(mode)
+    }
+
+    override fun bindIsolatedService(
+        service: Intent,
+        flags: Int,
+        instanceName: String,
+        executor: Executor,
+        conn: ServiceConnection
+    ): Boolean {
+        return super.bindIsolatedService(service, flags, instanceName, executor, conn)
     }
 
 }
