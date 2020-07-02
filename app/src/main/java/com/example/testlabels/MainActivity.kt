3e142ea8cc30d1299a1fca734d564a71d4f9f495
giannispapageorgiou <g.papageorgiou@thebeat.co>
2020-07-02 11:35:13 +0300

    medium

--- app/src/main/java/com/example/testlabels/MainActivity.kt
+++ app/src/main/java/com/example/testlabels/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.testlabels
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
@@ -8,5 +9,51 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    //add lines
+    //add lines
+    //add lines
+    //add lines
+    //add lines
+    //add lines
+    //add lines
+    //add lines
+    //add lines
+    //add lines
+    //add lines
+    //add lines
+    //add lines
+    //add lines
+    //add lines
+    //add lines
+    //add lines
+    //add lines
+    //add lines
+    //add lines
+    //add lines
+    //add lines
+    //add lines
+    //add lines
+    //add lines
+    //add lines
+    //add lines
+    //add lines
+    //add lines
+    //add lines
+    //add lines
+    //add lines
+    //add lines
+    //add lines
+    //add lines
+    //add lines
+    //add lines
+    //add lines
+    //add lines
+    //add lines
+    //add lines
+
+    override fun applyOverrideConfiguration(overrideConfiguration: Configuration?) {
+        super.applyOverrideConfiguration(overrideConfiguration)
     }
 }
